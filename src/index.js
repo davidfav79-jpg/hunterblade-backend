e0d077375b5c75942cdfc1d6957d697197ac5cd2
davidfav79-jpg <davidfav79@gmail.com>
2026-05-08 17:48:06 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.json());
 
 app.use('/api/clientes',  require('./routes/clientes'));
